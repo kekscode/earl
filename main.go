ff3e73ae4853ac8926532d20cd49b993a39c5875
Stefan Antoni <stefan.antoni@deutschebahn.com>
2021-01-13 14:29:29 +0100

    Adding and showing a mark works now

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/url"
 	"time"
 )
 
 // Book represents a container for marks
 type Book struct {
-	Marks []Mark
+	Marks []*Mark
 }
 
 // Mark represents a persistent item
 type Mark struct {
-	URL     url.URL
+	URL     *url.URL
 	Title   string
 	Tags    []string
 	Comment string
 	Added   time.Time
 }
 
-func main() {}
+func (b *Book) ListMarks() {
+	fmt.Printf("%q", b.Marks)
+}
+func (b *Book) GetMark() {}
+func (b *Book) AddMark(m *Mark) {
+	b.Marks = append(b.Marks, m)
+}
+func (b *Book) ModifyMark() {}
+func (b *Book) DeleteMark() {}
+
+func main() {
+	b := Book{}
+	u, err := url.Parse("https://google.de")
+	if err != nil {
+		log.Fatalf("%q", err)
+	}
+	m := Mark{
+		URL:     u,
+		Title:   "Google Web Search",
+		Tags:    []string{"a", "ab", "abc", "blubber lutsch"},
+		Comment: "Blubber ist bla",
+		Added:   time.Now()}
+
+	b.AddMark(&m)
+	b.ListMarks()
+}
