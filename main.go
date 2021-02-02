55e8bb4c0c4e22b1aeda8570397e6fb56c2d258c
Stefan Antoni <stefan.antoni@deutschebahn.com>
2021-02-02 13:43:50 +0100

    Add signal handler for properly stopping serve

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/kekscode/earl/app/cmd"
 	"github.com/kekscode/earl/book"
 )
 
 func main() {
+
+	go func() {
+		sig := make(chan os.Signal)
+		signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+		s := <-sig
+		log.Fatalf("Signal (%v) received, stopping\n", s)
+	}()
 
 	book := book.New()
 	book.ReadFromJSON()
@@ -14,4 +26,5 @@ func main() {
 	book.SaveToJSON()
 
 	cmd.Execute()
+
 }
